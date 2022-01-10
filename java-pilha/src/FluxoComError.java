fbfdc65a34bb3a59ab79044ee3a7c7d2427a9d87
joaoBotelho22 <joaopedro@saudeagora.com.vc>
2022-01-10 14:02:39 -0300

    Checked e unchecked

--- java-pilha/src/FluxoComError.java
+++ java-pilha/src/FluxoComError.java
@@ -15,15 +15,14 @@ public class FluxoComError {
         System.out.println("Fim do main");
     }
 
-    private static void metodo1() {
+    private static void metodo1() throws MinhaException {
         System.out.println("Ini do metodo1");
         metodo2();
         System.out.println("Fim do metodo1");
     }
 
-    private static void metodo2() {
+    private static void metodo2() throws MinhaException {
     	System.out.println("Inicio método 2");
-    	metodo2();
-    	System.out.println("Fim método 2");
+    	throw new MinhaException("Deu muito errado");
     }
 }
