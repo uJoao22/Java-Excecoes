6473b98712eca52d198e7aceec3c38b18b7f5faa
joaoBotelho22 <joaopedro@saudeagora.com.vc>
2022-01-11 10:11:01 -0300

    Try with resources

--- java-pilha/src/TesteConexao.java
+++ java-pilha/src/TesteConexao.java
@@ -2,6 +2,20 @@ public class TesteConexao {
 
 	public static void main(String[] args) {
 
+		
+		//Instanciando a conexão através do try, de tal forma ele precisa assinar o contrato, implementar a interface de 
+		//AutoCloseable e após o teste do try ele já implementa o finally com o método close por debaixo dos panos
+		try(Conexao con = new Conexao()){ 
+			con.leDados(); //Testando o método leDados
+		} catch(IllegalStateException ex) {
+			System.out.println("Deu erro na conexão");
+		}
+		
+		
+		// ---------------------------------------------- 
+		
+		/*
+		
 		Conexao con = null;
 		try {
 			con = new Conexao();
@@ -9,8 +23,10 @@ public class TesteConexao {
 		} catch(IllegalStateException ex) {
 			System.out.println("Deu erro na conexão");
 		} finally {
-			con.fecha();
+			con.close();
 		}
+		
+		*/
 		
 	}
 	
